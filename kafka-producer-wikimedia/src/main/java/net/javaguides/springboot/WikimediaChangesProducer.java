e5cb6013559f2f35a7932179450b430a6e93565f
danisherror <danishmsrit780@gmail.com>
2025-10-04 13:05:17 +0530

    adding first commit

--- kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/WikimediaChangesProducer.java
@@ -1,9 +1,14 @@
 package net.javaguides.springboot;
 
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.URL;
 
 @Service
 public class WikimediaChangesProducer {
@@ -20,6 +25,10 @@ public class WikimediaChangesProducer {
 
         String topic = "wikimedia_recentchange";
         // to read real time stream data from wiki media we use event source
+        BackgroundEventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url="https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler,URI.create(url));
+
 
 
     }
